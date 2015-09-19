505730340adb0d942325b281999aa1c19a3d1c19
Owen Tanner Wilkerson <tanner.wilkerson@gmail.com>
2015-09-19 19:18:27 -0400

    Create Savings.h

---
+++ Bank-Account-Simulator/Savings.h
@@ -0,0 +1,31 @@
+//course: CS215-005
+//Project: Lab Assignment 11
+//Date: 04/24/2015
+//Purpose: to simulate bank account transactions
+//Author: Owen Tanner Wilkerson
+
+#ifndef Savings_H
+#define Savings_H
+
+#include <iostream>
+#include "Account.h"
+
+using namespace std;
+
+class Savings : public Account
+{
+public:
+	//default constructor
+	Savings();
+
+	//constructor
+	Savings(double balance);
+
+	//calculates the daily interest of the account balance
+	void daily_interest();
+
+	//prints out the account balance
+	void display_balance();
+private:
+};
+#endif
